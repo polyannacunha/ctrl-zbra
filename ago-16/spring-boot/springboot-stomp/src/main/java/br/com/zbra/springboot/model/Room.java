516b3d3c39775be4a8ffb90124366c6043535736
Caio Maia <caio.maia@zbra.com.br>
2016-07-27 20:43:50 -0300

    Room model completion

--- ago-16/spring-boot/springboot-stomp/src/main/java/br/com/zbra/springboot/model/Room.java
+++ ago-16/spring-boot/springboot-stomp/src/main/java/br/com/zbra/springboot/model/Room.java
@@ -1,11 +1,36 @@
 package br.com.zbra.springboot.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Room implements Serializable {
     private static final long serialVersionUID = 8172436800457489384L;
     private Long id;
     private String name;
+    private Set<User> users;
+    private List<Message> messages;
+
+    public Room() {
+        this.users = new HashSet<>();
+        this.messages = new ArrayList<>();
+    }
+
+    public Room(Long id, String name, Set<User> users, List<Message> messages) {
+        this.id = id;
+        this.name = name;
+        this.users = users;
+        this.messages = messages;
+    }
+
+    public Room(Long id, String name) {
+        this.id = id;
+        this.name = name;
+        this.users = new HashSet<>();
+        this.messages = new ArrayList<>();
+    }
 
     public Long getId() {
         return id;
@@ -21,5 +46,33 @@ public class Room implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+
+    public void addUser(User user) {
+        this.users.add(user);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Room room = (Room) o;
+
+        return id != null ? id.equals(room.id) : room.id == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
