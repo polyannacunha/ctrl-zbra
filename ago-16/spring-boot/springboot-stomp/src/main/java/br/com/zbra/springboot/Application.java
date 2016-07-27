43f9e222ad7a797cd69c72c6a79b048c3c71e0bd
Caio Maia <caio.maia@zbra.com.br>
2016-07-27 20:44:11 -0300

    Default app configuration

--- ago-16/spring-boot/springboot-stomp/src/main/java/br/com/zbra/springboot/Application.java
+++ ago-16/spring-boot/springboot-stomp/src/main/java/br/com/zbra/springboot/Application.java
@@ -1,9 +1,18 @@
 package br.com.zbra.springboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@ComponentScan({"br.com.zbra.springboot.configuration",
+        "br.com.zbra.springboot.repository",
+        "br.com.zbra.springboot.service",
+        "br.com.zbra.springboot.controller"})
+@EnableAutoConfiguration
+@EnableAsync
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
